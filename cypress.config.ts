980146bcda7dc5c314490f8b2c532f2ea24606ec
shun2218-dev <kohaku1015game@gmail.com>
2023-02-23 09:39:59 +0900

    change cofig files

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://realtime-app-supabase-omega.vercel.app/',
     viewportHeight: 600,
     viewportWidth: 1000,
+    video: true,
+    trashAssetsBeforeRuns: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    reactRemoveProperties: false,
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
 }
 
 module.exports = nextConfig
